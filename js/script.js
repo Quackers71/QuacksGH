9f97e02fc20586f31cd12da30651b51a43880777
QuackQuack <iburger71@gmail.com>
2018-07-10 21:49:14 +0100

    Logical Operators short version

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 
 //Logical Operators && and ||
 
+let day = "monday";
+let money = 53;
 
+if ( day === "friday" &&  money > 50) {
+	console.log("You are going out!");
+}
+else {
+	console.log("Your not going out");
+} 
+	
 
 
 
