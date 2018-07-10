a25922ddb4c33d93ebd9a8ac02242c309e9d2298
Robert Qarikau <RQarikau@NCSCCOBALTWINDOWS.GOV>
2018-07-10 13:45:51 +0100

    Taster of Objects

--- js/script.js
+++ js/script.js
@@ -1,11 +1,42 @@
 
-//Declare, Assignment, Operator, Assign Value;
+//Objects in JS
 
-//Variables
+let person = {
+    
+    name: "John",
+    lastName: "Pepper",
+    age: 25,
+    education: false,
+    wife: true,
+    siblings: ['sister','brother'],
+    fullName: function() {
+        console.log("And the full name of the person is : " + person.name + " " + person.lastName )
+    },
+    describePerson: function(){ 
+    
+        console.log( person.name + " is " + person.age + " and has " + person.siblings.length + " Siblings" );
+
+        console.log( person.name + " also has education : " + person.education );
+        
+        console.log( person.name + " is married : " + person.wife );
+        
+    }
+}
+
+let personName = person.name;
+let personLastName = person['lastName'];
+
+console.log(personName);
+console.log(personLastName);
+
+person.fullName();
+
+console.log(person.age);
+
+person.describePerson();
 
 
-const Name = "Quackers Jnr II";
-//Name = "Quacks Yo";
 
-console.log(Name);
+
+
 
