f954461c73cc017ef2a2e4c68103e2519cae0468
QuackQuack <iburger71@gmail.com>
2018-07-10 22:35:23 +0100

    Turnary Operator

--- js/script.js
+++ js/script.js
@@ -1,2 +1,26 @@
-//
+//unary operator
+
+let text = "string";
+
+console.log(typeof text); //operand
+
+//binary operator
+
+let number = 3 + 5;
+
+//ternary
+
+//condition ? (runs if true) : (runs if false)
+
+let result = 10;
+
+//result % 2 === 0 ? console.log("even number") : console.log("odd number");
+
+let expression = result % 2;
+
+function response (text) {
+	console.log(text + " number");
+}
+
+expression === 0 ? response("even") : response("odd"); 
 
