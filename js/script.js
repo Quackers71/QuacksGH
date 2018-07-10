95310fc1b0bd8294d9c05e547e99a6aa479cccc9
Robert Qarikau <RQarikau@NCSCCOBALTWINDOWS.GOV>
2018-07-10 15:20:20 +0100

    a bit of ifelse work

--- js/script.js
+++ js/script.js
@@ -4,6 +4,22 @@
 //COMPARISON OPERATORS
 //>,<,>=,==,===,!=,!==
 
+let person1 = "John";
+let person2 = "Bob";
+
+let person = "Peter";
+
+if (person === "John") {
+    console.log("This is deffo John");
+}
+else if (person === "Bob"){
+         console.log("This dude is Bob ");
+         }
+
+else {
+    console.log("Who is this dude!");
+}
+
 
 
 
