5568a00b394ea7159ad7f730129407af923288e3
QuackQuack <iburger71@gmail.com>
2018-07-11 21:20:45 +0100

    switch exercise

--- js/script.js
+++ js/script.js
@@ -1,2 +1,39 @@
-//
+// Switch Exercise
 
+function displayDayOfTheWeek(day) {
+	
+	switch(day) {
+		case 1:
+			console.log("Today is Monday");
+			break;
+		case 2:
+			console.log("Today is Tuesday");
+			break;
+		case 3:
+			console.log("Today is Wednesday");
+			break;
+		case 4:
+			console.log("Today is Thursday");
+			break;
+		case 5:
+			console.log("Today is Friday");
+			break;
+		case 6:
+			console.log("Today is Saturday");
+			break;
+		case 7:
+			console.log("Today is Sunday");
+			break;
+		default:
+			console.log("Invalid Day!");
+	}
+}
+
+displayDayOfTheWeek(4);
+displayDayOfTheWeek(3);
+displayDayOfTheWeek(2);
+displayDayOfTheWeek(1);
+displayDayOfTheWeek(6);
+displayDayOfTheWeek(5);
+displayDayOfTheWeek(7);
+displayDayOfTheWeek(0);
