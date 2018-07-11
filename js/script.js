567fc624d60ac7a6c37efffa1dd4b6b648f7e2d6
QuackQuack <iburger71@gmail.com>
2018-07-12 00:23:10 +0100

    Value Reference

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
-//
+//Primitive Data Types
+//String, Number, Symbol, Boolean, Undefined, Null
+//Arrays, Functions, Objects = object
+//(typeof)
+
+//assigning to primitive value any changes
+//are made directly to that value
+
+//assigning non-primitive value to variable is done by reference
+//so any of the changes will affect all the references
+
+let number = 1;
+let number2 = number;
+
+console.log("The first value is " + number);
+console.log("The second value is " + number2);
 
