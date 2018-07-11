50020a423dc900f2a9b9cb6efdb5fe0dde8361a2
QuackQuack <iburger71@gmail.com>
2018-07-11 21:44:29 +0100

    While and Do While

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
-//
+//repeatedly run a block of code while certain condition is true
+//while loop
+
+/*
+let amount = 10;
+
+while(amount > 0) {
+	console.log("I have " + amount + " pounds and I'm going to the shops Yo!");
+	
+	amount--;
+}
+*/
+
+let money = 1;
+
+do {
+	console.log("You have " + money + " pounds and you are going to the shops Yo!");
+	money++;
+}while(money < 11);
+
 
