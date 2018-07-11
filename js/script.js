54cb6179d24914ae504ffcb71bd73026adb1ade9
QuackQuack <iburger71@gmail.com>
2018-07-11 21:52:37 +0100

    for loops

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
-//
+//repeatedly run a block of code while certain condition is true
+//for loop
+
+let i;
+
+for (i = 1; i <= 10; i++) {
+	console.log("And the number is " + i);
+}
+
+for (let n = 10; n >= 1; n--) {
+	console.log("And the number is " + n);
+}
 
