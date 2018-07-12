315e3775c9fd1b3e6da583474806ea836cad758c
Robert Qarikau <RQarikau@NCSCCOBALTWINDOWS.GOV>
2018-07-12 17:37:30 +0100

    String / Objects

--- js/script.js
+++ js/script.js
@@ -1,2 +1,43 @@
-//
+//String
+
+let name = "john";
+let lastName = "pepper";
+
+let fullName = name + " " + lastName; // concatination
+
+console.log(fullName);
+//Obj
+let person = {
+    name: "John", // properties
+    lastName: "Pepper",
+    fullName: function() {  // methods
+        console.log(this.name + " " + this.lastName )
+    }
+}
+console.log(person.name);
+person.fullName();
+
+// String obj
+
+let human = "John Pepper";
+
+let length = human.length;
+console.log(length);
+
+console.log(human.toLowerCase());
+console.log(human.toUpperCase());
+
+console.log(human.charAt(0));
+console.log(human.charAt(human.length-1));
+
+console.log(human.indexOf("e"));
+
+console.log(human.toLowerCase().startsWith("john"));
+console.log(human.toLowerCase().endsWith("pepper"));
+
+console.log(human.slice(0,5));
+console.log(human.slice(-5));
+
+console.log(human.toLowerCase().includes("john"));
+
 
