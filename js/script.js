c1443228d2da367624dae509ac47c19429f9f85b
Robert Qarikau <RQarikau@NCSCCOBALTWINDOWS.GOV>
2018-07-12 16:41:25 +0100

    Referencing Data Types

--- js/script.js
+++ js/script.js
@@ -11,7 +11,17 @@
 
 let number = 1;
 let number2 = number;
+number2 = 4;
 
 console.log("The first value is " + number);
 console.log("The second value is " + number2);
+
+
+let person = {name: 'bob'};
+let person2 = person;
+person2.name = "john";
+
+console.log("The name of the first person is " + person.name);
+console.log("The name of the second person is " + person2.name);
+
 
