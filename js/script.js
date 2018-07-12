250491d97dec44a1f65575c989773fa7c32a8a8c
Robert Qarikau <RQarikau@NCSCCOBALTWINDOWS.GOV>
2018-07-12 17:03:37 +0100

    Difference between null and undefined

--- js/script.js
+++ js/script.js
@@ -1,2 +1,23 @@
-//
+// both represent "no-value"
+
+// undefined ---- " javascript can not find value for this"
+
+// null -----"programmer set the value"
+
+
+let number;
+console.log(number);
+
+
+let number1 = null;
+console.log(number1);
+
+let number2 = 20 + null;
+console.log(number2);
+
+let number3 = 20 + undefined;
+console.log(number3);
+
+
+
 
