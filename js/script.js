68113a9d80299845c805d93f88160b654d156183
QuackQuack <iburger71@gmail.com>
2018-07-13 09:39:07 +0100

    String Templates

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
-//
+//String Templates
+
+let name = "john";
+let age = "25";
+let sentence = `Hey it's ${name} and he's ${age} years old`;
+
+//console.log("Hey it's " + name + " and he's "+ age + " years old");
+
+console.log(sentence);
 
