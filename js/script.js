218d616ac794d31da550d1ac7ec42700e81d55aa
Quackers <24840026+Quackers71@users.noreply.github.com>
2018-07-13 17:35:07 +0100

    Arrays and For Loops

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
-//
+//for loop
 
+//for(let i = 1; i <= 10; i++) {
+//    console.log(`Your number ${i}`);
+//}
+
+let friends = ['bob','suzy','john','eddie','olga'];
+
+//for (let i = 0; i < friends.length; i++) {
+//	console.log(`Your friend number ${i+1} is ${friends[i]}`);
+//}
+
+for (let i = friends.length-1; i >= 0; i--) {
+	console.log(`Your friend number ${i+1} is ${friends[i]}`);
+}
