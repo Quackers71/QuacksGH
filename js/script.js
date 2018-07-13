13f96a60df7a80e6bd70e97821258da3f1b15d75
Quacks <24840026+Quackers71@users.noreply.github.com>
2018-07-13 23:00:25 +0100

    Math Objects

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
-//
+// Global Math Object
+
+let number = 4678;
+
+//let result = Math.floor(number);
+//let result = Math.ceil(number);
+//let result = Math.sqrt(number);
+//let result = Math.PI;
+//let result = Math.min(1,8,4,5,7);
+//let result = Math.max(1,8,4,5,7);
+let result = Math.floor((Math.random()*10)+1);
+
+
+console.log(result);
 
